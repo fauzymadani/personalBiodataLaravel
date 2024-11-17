735d332c711a28bdb186eae0f73c8f0164ae8fe7
fauzymadani <keperluansekolahfauzy@gmail.com>
2024-11-17 14:22:33 +0700

    add more new feature

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portofolio</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <style>
 
     @font-face {
@@ -149,6 +150,7 @@
             color: #fff;
             text-transform: uppercase;
             font-weight: bold;
+            margin-top: 20px;
         }
 
         tr:hover {
@@ -173,7 +175,8 @@
         }
 
         .skill-bar {
-            margin: 10px 0;
+            margin: 10px;
+            margin-bottom: 20px;
         }
 
         .skill-bar p {
@@ -194,6 +197,46 @@
             transition: width 0.5s ease-in-out;
         }
 
+        /* github.com/alfredsaveron/scrollbars/Block */
+        ::-webkit-scrollbar-track
+        {
+            -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+            background-color: #F5F5F5;
+        }
+
+        ::-webkit-scrollbar
+        {
+            width: 9px;
+            background-color: #F5F5F5;
+        }
+
+        ::-webkit-scrollbar-thumb
+        {
+            background-color: #F90;
+            background-image: -webkit-linear-gradient(90deg,
+	                                          rgba(255, 255, 255, .2) 25%,
+											  transparent 25%,
+											  transparent 50%,
+											  rgba(255, 255, 255, .2) 50%,
+											  rgba(255, 255, 255, .2) 75%,
+											  transparent 75%,
+											  transparent)
+        }
+
+        .map {
+            height: 240px;
+        }
+
+        .ext-list {
+            margin: 10px;
+        }
+
+        .debian {
+              display: block;
+              margin-left: auto;
+              margin-right: auto;
+              width: 50%;
+        }
 
         @media (max-width: 768px) {
             .portfolio-container {
@@ -208,8 +251,16 @@
             p {
                 font-size: 1rem;
             }
+
+            .map {
+                height: 200px;
+            }
         }
     </style>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/atom-one-dark.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
+    <link rel="stylesheet" href="{{ asset('css/dracula.css') }}">
 </head>
 <body>
     <div class="portfolio-container">
@@ -257,7 +308,7 @@
             one day, i'm bored and just watching a youtube video about why you should move from windows to Linux. and that is inspiring me to switch to linux.
             my first command in linux was <pre>sudo apt update</pre> i love Debian so much than the ohers distro's.</p>
         <h2>my first C project</h2>
-        <pre>
+        <pre><code class="language-c">
 // Author: Fauzy Madani
 // https://github.com/fauzymadani
 
@@ -313,8 +364,12 @@ int main() {
 
     return 0;
 }
+        </code>
         </pre>
-
+        <p>
+        When I’m not immersed in my projects, I find joy in sharing knowledge with peers, contributing to open-source communities, and constantly pushing the boundaries of what technology can achieve. Inspired by the power of collaboration and innovation, I dream of creating impactful digital solutions that make a difference in the lives of others. Whether it’s customizing a Linux distro or perfecting a website design, I embrace every challenge with determination, proving that learning and growing are as limitless as the open-source universe I admire.
+        </p>
+        <hr>
         <div class="portfolio-container">
             <h2>## Favourite Tools</h2>
             <table>
@@ -347,7 +402,7 @@ int main() {
 
         <h2>### Exploring Programming Language</h2>
         <p>my current skill at programing is
-        <pre>
+        <pre><code class="language-bash">
 From: 29 October 2024 - To: 15 November 2024
 
 Total Time: 28 hrs 13 mins
@@ -365,27 +420,114 @@ CSS              17 mins         ▒░░░░░░░░░░░░░░�
 
 <span>this is not my full time, i just installed wakatime plugin in neovim only,
 i use zed, nano, vim, and the other IDE</span>
+        </code>
         </pre>
         </p>
         <div class="skill-bar">
-            <p>PHP</p>
+            <p>Linux Enthusiast🐧</p>
             <div class="bar">
-                <div class="fill" style="width: 38%;"></div>
+                <div class="fill" style="width: 100%;"></div>
             </div>
         </div>
         <div class="skill-bar">
-            <p>conf</p>
+            <p>Web Developer 👨‍💻</p>
             <div class="bar">
-                <div class="fill" style="width: 16%;"></div>
+                <div class="fill" style="width: 95%;"></div>
             </div>
         </div>
-
-
+        <div class="skill-bar">
+            <p>Fighter🥊</p>
+            <div class="bar">
+                <div class="fill" style="width: 70%;"></div>
+            </div>
+        </div>
+        <div class="skill-bar">
+            <p>Student🧑‍🎓</p>
+            <div class="bar">
+                <div class="fill" style="width: 99%;"></div>
+            </div>
+        </div>
+        <p>i'm <span>Fauzy</span>. that's it, thak you for reading, now you know me</p>
+        <hr>
+        <h2>## I Live In Indonesia Btw</h2>
+        <p>Indonesia,[b] officially the Republic of Indonesia,[c] is a country in Southeast Asia and Oceania, between the Indian and Pacific oceans. It consists of over 17,000 islands, including Sumatra, Java, Sulawesi, and parts of Borneo and New Guinea. Indonesia is the world's largest archipelagic state and the 14th-largest country by area, at 1,904,569 square kilometres (735,358 square miles). With over 280 million people,
+            Indonesia is the world's fourth-most-populous country and the most populous Muslim-majority country. Java, the world's most populous island, is home to more than half of the country's population.<br>
+            <br>
+            <img src="{{ asset('image/indonesia.svg.png') }}" alt="Emacs Icon" class="map"><br>
+        Indonesia is a presidential republic with an elected legislature. It has 38 provinces, of which nine have special autonomous status. The country's largest city, Jakarta, is the world's second-most-populous urban area. Indonesia shares land borders with Papua New Guinea, East Timor, and the eastern part of Malaysia, as well as maritime borders with Singapore, Peninsular Malaysia, Vietnam, Thailand, the Philippines, Australia, Palau, and India. Despite its large population and densely populated regions, Indonesia has vast areas of wilderness that support one of the world's highest levels of biodiversity.</p>
+        <table class="custom-table">
+            <thead>
+                <tr>
+                    <th>Population</th>
+                    <th>more than 270 million across 17,000 islands</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Size</td>
+                    <td>7.9 million km²</td>
+                </tr>
+                <tr>
+                    <td>Landscapes</td>
+                    <td>Mountain, Rice field, Sea</td>
+                </tr>
+                <tr>
+                    <td>Economy</td>
+                    <td>Agriculture, mining, farming</td>
+                </tr>
+                <tr>
+                    <td>Religion</td>
+                    <td>Islam, Christiany, Budhism, Tionghoa(chinese), Hinduism, Catholic</td>
+                </tr>
+            </tbody>
+        </table>
+        <hr>
+        <h2>## Supported Extension</h2>
+        <div class="ext-list">
+            <ul>
+                <li>*.c</li>
+                <li>*.blade.php</li>
+                <li>*.php</li>
+                <li>*.css</li>
+                <li>*.lua</li>
+                <li>*.html</li>
+                <li>*.js</li>
+            </ul>
+        </div>
+        <hr>
+        <h2>## Why Using Linux? ## </h2>
+        <div class="debian">
+        <img src="{{ asset('image/openlogo.svg') }}" align="center" class="debian-image">
+        </div>
+        <p>many people have questioned me, "Why are you using Linux? Linux is hard, just use windows!"
+            the answer's is i don't have money to go outside, to playing or <span>"nongkrong"</span> with my friends. so i'm
+            trying not too many interactions with my friends. using linux is the best choice so i'm having more time
+            configuring linux, customizing it, trying window manager, text editor, looking for inspiration for linux themes.
+            and since i don't have any girlfriend, my time is fully on linux and programming. (i have interest on unix and freebsd too)</p>
+        <hr>
+        <h2>## What people say!!</h2>
+        <script src="https://utteranc.es/client.js"
+                repo="fauzymadani/personalBiodataLaravel"
+                issue-term="pathname"
+                label="comment"
+                theme="github-dark"
+                crossorigin="anonymous"
+                async>
+        </script>
 
         @else
             <p class="error-message">The Data Is Not Available, Try Again Later..</p>
         @endif
         <footer><p>Manual page Fauzy(1) (press h for help)</p></footer>
     </div>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/atom-one-dark.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', (event) => {
+            hljs.highlightAll();
+        });
+    </script>
+    <link rel="stylesheet" href="{{ asset('css/dracula.css') }}">
+
 </body>
 </html>
